f5e9f1e235a5ab692ca6790abafb7f6405a65dd8
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2024-10-15 13:25:18 +0200

    + | free ok, integration test missing

--- Malloc/SRCS/ft_free.c
+++ Malloc/SRCS/ft_free.c
@@ -1,5 +1,9 @@
 #include "ft_malloc.h"
 
 void	ft_free(void *ptr) {
-    /*TODO*/
+  	if (!ptr)
+          return;
+    t_block	*block = (t_block *)ptr - sizeof(t_block);
+    block->free = 1;
+    merge_blocks(block);
 }
