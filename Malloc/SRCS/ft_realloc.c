0db6bd63ab10d1dfcd753ebaee006e86a52e0f67
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2024-10-15 13:35:33 +0200

    + | realloc ok, integration test missing

--- Malloc/SRCS/ft_realloc.c
+++ Malloc/SRCS/ft_realloc.c
@@ -1,5 +1,22 @@
 #include "ft_malloc.h"
 
 void	*ft_realloc(void *ptr, size_t size) {
-    /*TODO*/
+    if (!ptr)
+        return ft_malloc(size);
+    if (!size) {
+        ft_free(ptr);
+        return MALLOC_FAILED;
+    }
+    t_block *block = (t_block *)ptr - sizeof(t_block);
+    if (block->size >= size) {
+        if (block->size > size + sizeof(t_block))
+            split_block(block, size);
+        return ptr;
+    }
+    void *new_ptr = ft_malloc(size);
+    if (!new_ptr)
+      return MALLOC_FAILED;
+    memcpy(new_ptr, ptr, size);
+    ft_free(ptr);
+    return new_ptr;
 }
