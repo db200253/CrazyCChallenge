88a31acc812d3b9845112e0dd9554798713fe5da
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2024-10-15 13:28:08 +0200

    + | calloc ok, integration test missing

--- Malloc/SRCS/ft_calloc.c
+++ Malloc/SRCS/ft_calloc.c
@@ -1,5 +1,10 @@
 #include "ft_malloc.h"
 
 void	*ft_calloc(size_t nmemb, size_t size) {
-    /*TODO*/
+    size_t s = nmemb * size;
+    void *ptr = ft_malloc(s);
+    if (!ptr)
+      return MALLOC_FAILED;
+    memset(ptr, 0, s);
+    return ptr;
 }
